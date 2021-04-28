2646df0158e19aadd1ade1c29f2baa95c2f41770
Robert Schade <roscha80@web.de>
2021-04-28 15:59:22 +0200

    Color change via js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 console.clear()
 
 const form = document.querySelector('[data-form]')
-const remove = document.querySelector('[data-remove]')
-const input = document.querySelector('[name="classes"]')
-const output = document.querySelector('[data-output]')
-
+const input = document.querySelector('[data-input]')
 const box = document.querySelector('[data-box]')
 
 form.addEventListener('submit', event => {
   event.preventDefault()
-  box.className = 'box ' + input.value
-  const inputText = input.value
+  const farbe = input.value
+  box.style.backgroundColor = farbe
   input.value = ''
   input.focus()
-  const outputText = inputText.toUpperCase()
-  output.value = outputText
-})
-
-remove.addEventListener('click', event => {
-  event.preventDefault()
-  box.classList.remove(input.value)
 })
