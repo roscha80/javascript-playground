66bb17a187602bc54e162787c6a3a0a3ff9ec623
Robert Schade <roscha80@web.de>
2021-04-28 11:08:08 +0200

    Saved

--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
 console.clear()
-const box = document.querySelector('.box')
-const buttonToggleRotate = document.querySelector('.button-rotate')
-const buttonToggleBackground = document.querySelector('.button-background')
-const buttonToggleRadius = document.querySelector('.button-radius')
-const buttonToggleAll = document.querySelector('.button-all')
+const box1 = document.querySelector('.box1')
+const box2 = document.querySelector('.box2')
+const box3 = document.querySelector('.box3')
+const box4 = document.querySelector('.box4')
+const buttonBox1 = document.querySelector('.button-box1')
+const buttonBox2 = document.querySelector('.button-box2')
+const buttonBox3 = document.querySelector('.button-box3')
+const buttonBox4 = document.querySelector('.button-box4')
 
-buttonToggleRotate.addEventListener('click', () => {
-  box.classList.toggle('box-rotate')
+buttonBox1.addEventListener('click', () => {
+  box1.classList.add('showtime')
+  box2.classList.remove('showtime')
+  box3.classList.remove('showtime')
+  box4.classList.remove('showtime')
 })
 
-buttonToggleBackground.addEventListener('click', () => {
-  box.classList.toggle('body-background')
+buttonBox2.addEventListener('click', () => {
+  box2.classList.add('showtime')
+  box1.classList.remove('showtime')
+  box3.classList.remove('showtime')
+  box4.classList.remove('showtime')
 })
 
-buttonToggleRadius.addEventListener('click', () => {
-  box.classList.toggle('box-radius')
+buttonBox3.addEventListener('click', () => {
+  box3.classList.add('showtime')
+  box1.classList.remove('showtime')
+  box2.classList.remove('showtime')
+  box4.classList.remove('showtime')
 })
 
-buttonToggleAll.addEventListener('click', () => {
-  box.classList.toggle('box-radius')
-  box.classList.toggle('body-background')
-  box.classList.toggle('box-rotate')
+buttonBox4.addEventListener('click', () => {
+  box4.classList.add('showtime')
+  box1.classList.remove('showtime')
+  box2.classList.remove('showtime')
+  box3.classList.remove('showtime')
 })
