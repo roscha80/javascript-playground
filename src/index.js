cf2eb267d8b6dec69437962af4db16a7eb2ee209
Robert Schade <roscha80@web.de>
2021-04-28 14:38:05 +0200

    input/out value

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ console.clear()
 const form = document.querySelector('[data-form]')
 const remove = document.querySelector('[data-remove]')
 const input = document.querySelector('[name="classes"]')
+const output = document.querySelector('[data-output]')
 
 const box = document.querySelector('[data-box]')
 
 form.addEventListener('submit', event => {
   event.preventDefault()
   box.className = 'box ' + input.value
+  const inputText = input.value
+  input.value = ''
   input.focus()
+  const outputText = inputText.toUpperCase()
+  output.value = outputText
 })
 
 remove.addEventListener('click', event => {
