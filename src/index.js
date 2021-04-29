287bf4c5b6e5fe0b125d2f3f16bd64de9cf1f084
Robert Schade <roscha80@web.de>
2021-04-29 09:27:16 +0200

    JS Range controlled transformation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 console.clear()
 
-const form = document.querySelector('[data-form]')
-const input = document.querySelector('[data-input]')
 const box = document.querySelector('[data-box]')
+const inputRotate = document.querySelector('[js__rotate]')
+const inputScale = document.querySelector('[js__scale]')
+const inputRadius = document.querySelector('[js__radius]')
+const inputColor = document.querySelector('[js__color]')
 
-form.addEventListener('submit', event => {
+inputRotate.addEventListener('input', event => {
   event.preventDefault()
-  const farbe = input.value
-  box.style.backgroundColor = farbe
-  input.value = ''
-  input.focus()
+  box.style.transform = 'rotate(' + inputRotate.value + 'deg)'
+})
+
+inputScale.addEventListener('input', event => {
+  event.preventDefault()
+  box.style.transform = 'scale(' + inputScale.value + '%)'
+})
+
+inputRadius.addEventListener('input', event => {
+  event.preventDefault()
+  box.style.borderRadius = inputRadius.value + 'px'
+})
+
+inputColor.addEventListener('input', event => {
+  event.preventDefault()
+  box.style.backgroundColor = 'hsl(' + inputColor.value + 'deg, 100, 100)'
+  console.log('hsl(' + inputColor.value + 'deg, 100, 100)')
 })
