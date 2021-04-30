841e849686e6cc7ad91e16171c13d75e404f7246
Robert Schade <roscha80@web.de>
2021-04-30 10:18:02 +0200

    JS exercises

--- src/index.js
+++ src/index.js
@@ -7,22 +7,20 @@ const inputRadius = document.querySelector('[js__radius]')
 const inputColor = document.querySelector('[js__color]')
 
 inputRotate.addEventListener('input', event => {
-  event.preventDefault()
   box.style.transform = 'rotate(' + inputRotate.value + 'deg)'
 })
 
 inputScale.addEventListener('input', event => {
-  event.preventDefault()
   box.style.transform = 'scale(' + inputScale.value + '%)'
 })
 
 inputRadius.addEventListener('input', event => {
-  event.preventDefault()
   box.style.borderRadius = inputRadius.value + 'px'
 })
 
+
+
 inputColor.addEventListener('input', event => {
-  event.preventDefault()
-  box.style.backgroundColor = 'hsl(' + inputColor.value + 'deg, 100, 100)'
-  console.log('hsl(' + inputColor.value + 'deg, 100, 100)')
+  box.style.background = 'hsl(' + inputColor.value + 'deg, 100%, 50%)'
+  console.log(box.style.background)
 })
